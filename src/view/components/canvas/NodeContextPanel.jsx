942d61804875b391d5f39037d5bbc71164800682
Tomasz Zajac <83421840+tomasz-zajac-volantis@users.noreply.github.com>
2021-06-12 23:03:08 +0200

    Feature/node context menu (#86)
    * node context menu added
    
    * node context menu added
    
    * small adjustments

---
+++ src/view/components/canvas/NodeContextPanel.jsx
@@ -0,0 +1,161 @@
+import * as React from 'react';
+import Box from '@material-ui/core/Box';
+import { Tooltip } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded';
+import ZoomInRoundedIcon from '@material-ui/icons/ZoomInRounded';
+import ZoomOutRoundedIcon from '@material-ui/icons/ZoomOutRounded';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import LinkOffIcon from '@material-ui/icons/LinkOff';
+import LinkIcon from '@material-ui/icons/Link';
+import Chip from '@material-ui/core/Chip';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import {
+  DIAGRAM_ENTITY_DELETED,
+  DIAGRAM_LINK_TARGET_SELECTED_EVENT,
+  DIAGRAM_NODE_COLLAPSED,
+  DIAGRAM_NODE_DETACHED,
+  DIAGRAM_NODE_EXPANDED,
+} from '../../diagram/consts';
+
+const useStyles = (width) =>
+  makeStyles({
+    panel: {
+      position: 'absolute',
+      top: 0,
+      left: width,
+    },
+  });
+
+const NodeContextPanel = ({ node }) => {
+  const classes = useStyles(node.size.width)();
+  return (
+    <Box className={classes.panel}>
+      {node.isSelected() && !node.options.isExpanded && node.options.isParent && (
+        <Tooltip title="Expand Node" aria-label="expand">
+          <IconButton
+            size="small"
+            className="controlEl"
+            onClick={() => {
+              node.fireEvent(
+                {
+                  id: node.getID(),
+                },
+                DIAGRAM_NODE_EXPANDED
+              );
+            }}
+          >
+            <ZoomInRoundedIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+      {node.isSelected() && node.options.isExpanded && node.options.isParent && (
+        <Tooltip title="Collapse Node" aria-label="collapse">
+          <IconButton
+            size="small"
+            className="controlEl"
+            onClick={() => {
+              node.fireEvent(
+                {
+                  id: node.getID(),
+                },
+                DIAGRAM_NODE_COLLAPSED
+              );
+            }}
+          >
+            <ZoomOutRoundedIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+      {node.options.possibleRelations &&
+        node.options.possibleRelations.types.map((type) => (
+          <Box key={type} m={1}>
+            <Tooltip title={`Create a ${type} relation`}>
+              <Chip
+                color="primary"
+                icon={<LinkIcon />}
+                key={type}
+                className="controlEl"
+                label={type}
+                size="small"
+                onClick={() => {
+                  node.fireEvent(
+                    {
+                      id: node.options.possibleRelations.id,
+                      source: node.options.possibleRelations.source,
+                      target: node.getID(),
+                      type,
+                    },
+                    DIAGRAM_LINK_TARGET_SELECTED_EVENT
+                  );
+                }}
+              />
+            </Tooltip>
+          </Box>
+        ))}
+      {node.isSelected() && node.parentNode && (
+        <Tooltip title="Detach from parent" aria-label="detach">
+          <IconButton
+            size="small"
+            className="controlEl"
+            onClick={() => {
+              node.fireEvent(
+                {
+                  id: node.getID(),
+                },
+                DIAGRAM_NODE_DETACHED
+              );
+            }}
+          >
+            <LinkOffIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+      {node.isSelected() && (
+        <Tooltip title="Delete from model" aria-label="delete">
+          <IconButton
+            size="small"
+            className="controlEl"
+            onClick={() => {
+              node.fireEvent(
+                {
+                  entity: node,
+                  deleteFromModel: true,
+                },
+                DIAGRAM_ENTITY_DELETED
+              );
+            }}
+          >
+            <DeleteForeverRoundedIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+      {node.isSelected() && (
+        <Tooltip title="Remove from view" aria-label="remove">
+          <IconButton
+            size="small"
+            className="controlEl"
+            onClick={() => {
+              node.fireEvent(
+                {
+                  entity: node,
+                  deleteFromModel: false,
+                },
+                DIAGRAM_ENTITY_DELETED
+              );
+            }}
+          >
+            <VisibilityOffIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Box>
+  );
+};
+
+NodeContextPanel.propTypes = {
+  node: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default NodeContextPanel;
--- src/view/diagram/nodes/RadicalComposedNodeWidget.jsx
+++ src/view/diagram/nodes/RadicalComposedNodeWidget.jsx
@@ -1,41 +1,28 @@
 import React from 'react';
 import { PortWidget } from '@projectstorm/react-diagrams';
 import Typography from '@material-ui/core/Typography';
-import LinkOffIcon from '@material-ui/icons/LinkOff';
-import IconButton from '@material-ui/core/IconButton';
-
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import Box from '@material-ui/core/Box';
 import { Divider } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import values from 'lodash/fp/values';
-import {
-  DIAGRAM_LINK_TARGET_SELECTED_EVENT,
-  DIAGRAM_NODE_COLLAPSED,
-  DIAGRAM_NODE_DETACHED,
-  DIAGRAM_NODE_EXPANDED,
-} from '../consts';
+
 import { getPortStyle } from '../helpers';
-import CollapsePanel from '../../components/canvas/CollapsePanel';
-import LinkingPanel from '../../components/canvas/LinkingPanel';
 import EditableLabel from '../../components/canvas/EditableLabel';
+import NodeContextPanel from '../../components/canvas/NodeContextPanel';
 
 const useStyles = makeStyles(() => ({
   smartPort: {
     width: '16px',
     height: '16px',
   },
-  linkingPanel: {
-    position: 'absolute',
-    width: '100%',
-    height: 30,
-    bottom: 0,
-    left: '5%',
-  },
   detachPanel: {
     position: 'absolute',
     width: 20,
     height: 20,
-    top: 0,
+    right: 15,
+    bottom: 10,
   },
 }));
 const RadicalComposedNodeWidget = ({
@@ -114,63 +101,12 @@ const RadicalComposedNodeWidget = ({
           </PortWidget>
         ))}
       </div>
-      <CollapsePanel
-        size={node.size}
-        isParent={node.options.isParent}
-        onExpand={() => {
-          node.fireEvent(
-            {
-              id: node.getID(),
-            },
-            DIAGRAM_NODE_EXPANDED
-          );
-        }}
-        onCollapse={() => {
-          node.fireEvent(
-            {
-              id: node.getID(),
-            },
-            DIAGRAM_NODE_COLLAPSED
-          );
-        }}
-      />
-      <div className={classes.linkingPanel}>
-        {node.options.possibleRelations &&
-          node.options.possibleRelations.types.map((type) => (
-            <LinkingPanel
-              key={type}
-              label={type}
-              onClick={() => {
-                node.fireEvent(
-                  {
-                    id: node.options.possibleRelations.id,
-                    source: node.options.possibleRelations.source,
-                    target: node.getID(),
-                    type,
-                  },
-                  DIAGRAM_LINK_TARGET_SELECTED_EVENT
-                );
-              }}
-            />
-          ))}
-      </div>
-      <div className={classes.detachPanel}>
-        {node.parentNode && (
-          <IconButton
-            className="controlEl"
-            onClick={() => {
-              node.fireEvent(
-                {
-                  id: node.getID(),
-                },
-                DIAGRAM_NODE_DETACHED
-              );
-            }}
-          >
-            <LinkOffIcon />
-          </IconButton>
-        )}
-      </div>
+      <NodeContextPanel node={node} />
+      {!node.isSelected() && !node.options.isExpanded && node.options.isParent && (
+        <Box className={classes.detachPanel}>
+          <AccountTreeIcon fontSize="small" />
+        </Box>
+      )}
     </div>
   );
 };

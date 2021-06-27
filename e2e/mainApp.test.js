c4a2d1378dbbe7055edcf0a5ba7f665957f5b5cb
Mateusz Wójcik <mateuszwojcik19@gmail.com>
2021-06-27 09:12:17 +0200

    Add basic file upload test

--- e2e/mainApp.test.js
+++ e2e/mainApp.test.js
@@ -1,8 +1,13 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
 
 describe('Basic flow', () => {
   it('Can open C4 model and see default view', async () => {
-    const browser = await puppeteer.launch({});
+    const browser = await puppeteer.launch({
+      // headless: false,
+      // devtools: true,
+      // slowMo: 250,
+    });
     const page = await browser.newPage();
 
     page.emulate({
@@ -21,6 +26,36 @@ describe('Basic flow', () => {
       () => document.querySelector('h6[data-testid=view-name]').textContent
     );
     expect(textContent).toBe('Default View');
+    browser.close();
+  }, 9000000);
+  it('Can open file and see view', async () => {
+    const browser = await puppeteer.launch({
+      // headless: false,
+      // devtools: true,
+      // slowMo: 250,
+    });
+    const page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 1024,
+        height: 768,
+      },
+      userAgent: '',
+    });
+    const filePath = path.relative(
+      process.cwd(),
+      path.join(__dirname, './test.radical')
+    );
+    await page.goto('http://localhost:3000/');
+    const loader = await page.waitForSelector('div[data-testid=file-uploader]');
+    const elementHandle = await loader.$('input[type=file]');
+    await elementHandle.uploadFile(filePath);
+    await page.waitForSelector('h6[data-testid=view-name]');
+    const textContent = await page.evaluate(
+      () => document.querySelector('h6[data-testid=view-name]').textContent
+    );
+    expect(textContent).toBe('Test view');
     browser.close();
   }, 9000000);
 });

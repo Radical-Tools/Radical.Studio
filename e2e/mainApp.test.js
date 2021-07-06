51b78e59890e530bf89a4abe7cd6fd39ff5c55da
Mateusz Wójcik <mateuszwojcik19@gmail.com>
2021-07-06 17:31:57 +0200

    test failed test

--- e2e/mainApp.test.js
+++ e2e/mainApp.test.js
@@ -2,79 +2,90 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 
+const timeout = 60000;
 describe('Basic flow', () => {
-  it('Can open C4 model and see default view', async () => {
-    const browser = await puppeteer.launch({
-      // headless: false,
-      // devtools: true,
-      slowMo: 50,
-    });
-    const page = await browser.newPage();
-    page.emulate({
-      viewport: {
-        width: 1024,
-        height: 768,
-      },
-      userAgent: '',
-    });
-    page
-      .on('console', (message) =>
-        console.log(
-          `${message.type().substr(0, 3).toUpperCase()} ${message.text()}`
+  it(
+    'Can open C4 model and see default view',
+    async () => {
+      const browser = await puppeteer.launch({
+        // headless: false,
+        // devtools: true,
+        slowMo: 50,
+      });
+      const page = await browser.newPage();
+      page.emulate({
+        viewport: {
+          width: 1024,
+          height: 768,
+        },
+        userAgent: '',
+      });
+      page
+        .on('console', (message) =>
+          console.log(
+            `${message.type().substr(0, 3).toUpperCase()} ${message.text()}`
+          )
         )
-      )
-      .on('pageerror', ({ message }) => console.log(message))
-      .on('requestfailed', (request) =>
-        console.log(`${request.failure().errorText} ${request.url()}`)
+        .on('pageerror', ({ message }) => console.log(message))
+        .on('requestfailed', (request) =>
+          console.log(`${request.failure().errorText} ${request.url()}`)
+        );
+      await page.goto('http://localhost:5000/');
+      await page.waitForSelector('button[data-testid=metamodel-selector-C4]');
+      await page.click('button[data-testid=metamodel-selector-C4sasa]');
+      await page.waitForSelector('h6[data-testid=view-name]');
+      const textContent = await page.evaluate(
+        () => document.querySelector('h6[data-testid=view-name]').textContent
       );
-    await page.goto('http://localhost:5000/');
-    await page.waitForSelector('button[data-testid=metamodel-selector-C4]');
-    await page.click('button[data-testid=metamodel-selector-C4]');
-    await page.waitForSelector('h6[data-testid=view-name]');
-    const textContent = await page.evaluate(
-      () => document.querySelector('h6[data-testid=view-name]').textContent
-    );
-    expect(textContent).toBe('Default View');
-    browser.close();
-  }, 9000000);
-  it('Can open file and see view', async () => {
-    const browser = await puppeteer.launch({
-      // headless: false,
-      // devtools: true,
-      slowMo: 50,
-    });
-    const page = await browser.newPage();
+      expect(textContent).toBe('Default View');
+      browser.close();
+    },
+    timeout
+  );
+  it(
+    'Can open file and see view',
+    async () => {
+      const browser = await puppeteer.launch({
+        // headless: false,
+        // devtools: true,
+        slowMo: 50,
+      });
+      const page = await browser.newPage();
 
-    page.emulate({
-      viewport: {
-        width: 1024,
-        height: 768,
-      },
-      userAgent: '',
-    });
-    page
-      .on('console', (message) =>
-        console.log(
-          `${message.type().substr(0, 3).toUpperCase()} ${message.text()}`
+      page.emulate({
+        viewport: {
+          width: 1024,
+          height: 768,
+        },
+        userAgent: '',
+      });
+      page
+        .on('console', (message) =>
+          console.log(
+            `${message.type().substr(0, 3).toUpperCase()} ${message.text()}`
+          )
         )
-      )
-      .on('pageerror', ({ message }) => console.log(message))
-      .on('requestfailed', (request) =>
-        console.log(`${request.failure().errorText} ${request.url()}`)
+        .on('pageerror', ({ message }) => console.log(message))
+        .on('requestfailed', (request) =>
+          console.log(`${request.failure().errorText} ${request.url()}`)
+        );
+      const filePath = path.relative(
+        process.cwd(),
+        path.join(__dirname, './test.radical')
       );
-    const filePath = path.relative(
-      process.cwd(),
-      path.join(__dirname, './test.radical')
-    );
-    await page.goto('http://localhost:5000/');
-    const loader = await page.waitForSelector('div[data-testid=file-uploader]');
-    const elementHandle = await loader.$('input[type=file]');
-    await elementHandle.uploadFile(filePath);
-    await page.waitForSelector('h6[data-testid=view-name]');
-    const textContent = await page.evaluate(
-      () => document.querySelector('h6[data-testid=view-name]').textContent
-    );
-    expect(textContent).toBe('Test view');
-    browser.close();
-  }, 9000000);
+      await page.goto('http://localhost:5000/');
+      const loader = await page.waitForSelector(
+        'div[data-testid=file-uploader]'
+      );
+      const elementHandle = await loader.$('input[type=file]');
+      await elementHandle.uploadFile(filePath);
+      await page.waitForSelector('h6[data-testid=view-name]');
+      const textContent = await page.evaluate(
+        () => document.querySelector('h6[data-testid=view-name]').textContent
+      );
+      expect(textContent).toBe('Test view');
+      browser.close();
+    },
+    timeout
+  );
 });

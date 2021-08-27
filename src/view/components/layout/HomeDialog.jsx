71a0ed115f2f8344a61fa876d3966786911482bc
Tomasz Zajac <tomasz.zajac@volantis.info>
2021-08-27 08:59:54 +0200

    formatting issue fix

--- src/view/components/layout/HomeDialog.jsx
+++ src/view/components/layout/HomeDialog.jsx
@@ -43,8 +43,6 @@ const useTitleStyles = makeStyles((theme) => ({
   },
 }));
 
-
-
 const Transition = React.forwardRef((props, ref) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <Slide direction="up" ref={ref} {...props} />

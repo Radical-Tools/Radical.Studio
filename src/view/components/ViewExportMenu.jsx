846a7dfbe27fea5e2dab2ca552d8f9f6b935bab0
Tomasz Zajac <tomasz.zajac@volantis.info>
2021-10-25 18:39:47 +0200

    svg export issue fixed

--- src/view/components/ViewExportMenu.jsx
+++ src/view/components/ViewExportMenu.jsx
@@ -29,7 +29,7 @@ const ViewExportMenu = ({ name }) => {
     };
 
     htmlToImage
-      .toSvg(document.querySelector('.DiagramWidget-view'), opt)
+      .toSvg(document.querySelector('.canvas-view'), opt)
       .then((dataUrl) => {
         download(dataUrl, `${name}.svg`);
       });

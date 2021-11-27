5293d136f2977433c302aa72c5eb93f93ae191f3
Tomasz Zajac <tomasz.zajac@volantis.info>
2021-11-27 16:47:14 +0100

    presentation and view flow updated

--- src/view/panels/PresentationsToolbarWidget.jsx
+++ src/view/panels/PresentationsToolbarWidget.jsx
@@ -1,12 +1,13 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import Box from '@material-ui/core/Box';
 import PropTypes from 'prop-types';
-import { Tooltip } from '@material-ui/core';
+import { Popover, Tooltip } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
 import PlayCircleFilledWhiteRoundedIcon from '@material-ui/icons/PlayCircleFilledWhiteRounded';
-import PresentationGridWidget from '../widgets/PresentationGridWidget';
 import { LAYOUT_MODE } from '../../app/consts';
+import TextFieldDebounced from '../components/TextFieldDebounced';
+import PresentationGridWidget from '../widgets/PresentationGridWidget';
 
 const PresentationsToolbarWidget = (props) => {
   const {
@@ -19,43 +20,79 @@ const PresentationsToolbarWidget = (props) => {
   } = props;
 
   const onCreatePresentationCallback = useCallback(
-    () => onCreatePresentation('New Presentation'),
+    (name) => onCreatePresentation(name),
     [onCreatePresentation]
   );
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'NewPresentation-Popover' : undefined;
+
   return (
-    <Box p={1} style={{ height: '100%' }}>
-      <PresentationGridWidget
-        presentationModel={presentationModel}
-        onUpdatePresentationName={onUpdatePresentationName}
-        onRemovePresentation={onRemovePresentation}
-        onPresentationActivate={onPresentationActivate}
-      />
-      <div style={{ position: 'absolute', top: 45, right: 30 }}>
-        <Tooltip title="Add New Presentation">
-          <IconButton size="small" onClick={onCreatePresentationCallback}>
-            <AddCircleRoundedIcon color="primary" />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Play Selected Presentation">
-          <span>
-            <IconButton
-              size="small"
-              onClick={() => onSetMode(LAYOUT_MODE.SHOW)}
-              disabled={presentationModel.current === undefined}
-            >
-              <PlayCircleFilledWhiteRoundedIcon
-                color={
-                  presentationModel.current !== undefined
-                    ? 'secondary'
-                    : 'inherit'
-                }
-              />
+    <>
+      <Box p={1} style={{ height: '100%' }}>
+        <PresentationGridWidget
+          presentationModel={presentationModel}
+          onUpdatePresentationName={onUpdatePresentationName}
+          onRemovePresentation={onRemovePresentation}
+          onPresentationActivate={onPresentationActivate}
+        />
+        <div style={{ position: 'absolute', top: 45, right: 30 }}>
+          <Tooltip title="Add New Presentation">
+            <IconButton size="small" onClick={handleClick}>
+              <AddCircleRoundedIcon color="primary" />
             </IconButton>
-          </span>
-        </Tooltip>
-      </div>
-    </Box>
+          </Tooltip>
+          <Tooltip title="Play Selected Presentation">
+            <span>
+              <IconButton
+                size="small"
+                onClick={() => onSetMode(LAYOUT_MODE.SHOW)}
+                disabled={presentationModel.current === undefined}
+              >
+                <PlayCircleFilledWhiteRoundedIcon
+                  color={
+                    presentationModel.current !== undefined
+                      ? 'secondary'
+                      : 'inherit'
+                  }
+                />
+              </IconButton>
+            </span>
+          </Tooltip>
+        </div>
+      </Box>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Box p={1}>
+          <TextFieldDebounced
+            initialValue="New Presentation"
+            label="type name and press enter"
+            onSubmit={(item) => {
+              handleClose();
+              onCreatePresentationCallback(item);
+            }}
+          />
+        </Box>
+      </Popover>
+    </>
   );
 };
 
--- src/view/panels/ViewsToolbarWidget.jsx
+++ src/view/panels/ViewsToolbarWidget.jsx
@@ -1,10 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import Box from '@material-ui/core/Box';
 import PropTypes from 'prop-types';
-import { Tooltip } from '@material-ui/core';
+import { Popover, Tooltip } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
+
 import ViewGridWidget from '../widgets/ViewGridWidget';
+import TextFieldDebounced from '../components/TextFieldDebounced';
 
 const ViewsToolbarWidget = (props) => {
   const {
@@ -17,30 +19,66 @@ const ViewsToolbarWidget = (props) => {
   } = props;
 
   const onUpsertViewCallback = useCallback(
-    () => onUpsertItem({ name: 'New View' }),
+    (name) => onUpsertItem({ name }),
     [onUpsertItem]
   );
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'NewView-Popover' : undefined;
+
   return (
-    <Box p={1} style={{ height: '100%' }}>
-      <ViewGridWidget
-        viewModel={viewModel}
-        current={current}
-        editMode={editMode}
-        onUpsertItem={onUpsertItem}
-        onRemoveView={onRemoveView}
-        onViewActivate={onViewActivate}
-      />
-      {editMode && (
-        <div style={{ position: 'absolute', top: 45, right: 30 }}>
-          <Tooltip title="Add New View">
-            <IconButton size="small" onClick={onUpsertViewCallback}>
-              <AddCircleRoundedIcon color="primary" />
-            </IconButton>
-          </Tooltip>
-        </div>
-      )}
-    </Box>
+    <>
+      <Box p={1} style={{ height: '100%' }}>
+        <ViewGridWidget
+          viewModel={viewModel}
+          current={current}
+          editMode={editMode}
+          onUpsertItem={onUpsertItem}
+          onRemoveView={onRemoveView}
+          onViewActivate={onViewActivate}
+        />
+        {editMode && (
+          <div style={{ position: 'absolute', top: 45, right: 30 }}>
+            <Tooltip title="Add View">
+              <IconButton size="small" onClick={handleClick}>
+                <AddCircleRoundedIcon color="primary" />
+              </IconButton>
+            </Tooltip>
+          </div>
+        )}
+      </Box>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Box p={1}>
+          <TextFieldDebounced
+            initialValue="New View"
+            label="type name and press enter"
+            onSubmit={(item) => {
+              handleClose();
+              onUpsertViewCallback(item);
+            }}
+          />
+        </Box>
+      </Popover>
+    </>
   );
 };
 

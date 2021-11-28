6e1e6ae2099332cc0c0f819de1d9adfb5152440f
Mateusz Wójcik <mateuszwojcik19@gmail.com>
2021-11-28 15:14:10 +0100

    Add only outgoing links descriptions

--- src/view/widgets/DiagramWidget/diagram/nodes/RadicalComposedNodeWidget.jsx
+++ src/view/widgets/DiagramWidget/diagram/nodes/RadicalComposedNodeWidget.jsx
@@ -63,9 +63,42 @@ const RadicalComposedNodeWidget = ({
       >
         {node.options.attributes?.description && (
           <Tooltip
-            title={node.options.attributes?.description}
+            title={
+              <div>
+                <Typography variant="subtitle1">Description:</Typography>
+                <div>
+                  <Typography variant="caption">
+                    {node.options.attributes?.description}
+                  </Typography>
+                </div>
+                <Typography variant="subtitle1">Outgoing Relations:</Typography>
+                <div>
+                  {node
+                    .getLinks()
+                    .filter(
+                      (element) =>
+                        element.getSourcePort().getParent().getID() ===
+                        node.getID()
+                    )
+                    .map((element) => (
+                      <div key={element.getID()}>
+                        <Typography variant="subtitle2" display="block">
+                          {`${element.getOptions().name} - ${
+                            element.getTargetPort().getParent().getOptions()
+                              .name
+                          }:`}
+                        </Typography>
+                        <Typography variant="caption" display="block">
+                          {element.getOptions().attributes?.description}
+                        </Typography>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            }
             aria-label="description"
             placement="right-start"
+            arrow
           >
             <VisibilityIcon
               sx={descriptionIconStyle(node.options.isExpanded)}

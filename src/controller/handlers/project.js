4f36c3b7ebdb080f0f77358daac200d24499bd5c
Mateusz Wójcik <mateuszwojcik19@gmail.com>
2022-03-20 10:12:29 +0100

    Add nested items for project

--- src/controller/handlers/project.js
+++ src/controller/handlers/project.js
@@ -1,13 +1,22 @@
 import set from 'lodash/fp/set';
+import * as presentations from './presentation';
+import * as history from './history';
+import * as model from './model';
+import * as viewModel from './viewModel';
 
 export const initialState = {
   project: {
     name: '',
     version: '',
     isLocked: false,
+    ...model.initialState,
+    ...viewModel.initialState,
+    ...presentations.initialState,
+    ...history.initialState,
   },
 };
-export const init = (state, payload) => set(['project'], payload, state);
+export const init = (state, payload) =>
+  set(['project'], { ...state.project, ...payload }, state);
 export const editName = (state, payload) =>
   set(['project', 'name'], payload.name, state);
 
